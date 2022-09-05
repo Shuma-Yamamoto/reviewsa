1c9209e3268c1d5c2086e07b0a8a1016676672f7
Shuma-Yamamoto <ebishuumai0743@gmail.com>
2022-09-05 13:10:46 +0900

    fix: deviseコントローラをデフォルトに戻した

--- app/controllers/university_students/registrations_controller.rb
+++ app/controllers/university_students/registrations_controller.rb
@@ -5,9 +5,9 @@ class UniversityStudents::RegistrationsController < Devise::RegistrationsControl
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource
   # def create
